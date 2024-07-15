1128abc6a75c533cef1ac9d4c61c2cae2db8b9d9
JadenC281 <jadencigola@gmail.com>
2024-07-15 11:56:59 -0400

    Structure stuffs

--- Wizard BattleSim/Assets/_Project_Files/Scripts/Interfaces/ISpell_Interface.cs
+++ Wizard BattleSim/Assets/_Project_Files/Scripts/Interfaces/ISpell_Interface.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public interface ISpell_Interface
 {
+<<<<<<< Updated upstream:Wizard BattleSim/Assets/_Project_Files/Scripts/Interfaces/ISpell_Interface.cs
     GameObject Caster { get; set; }
 
 
+=======
+    
+>>>>>>> Stashed changes:Wizard BattleSim/Assets/_Project_Files/Scripts/Interfaces/Spell_Interface.cs
 }
