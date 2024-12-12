84f5531baec5bab47f72006dd4f30b514ac758c5
Mr. Noob <jedstone567@gmail.com>
2024-12-12 02:44:54 -0500

    MOre selector screen

--- Wizard BattleSim/Assets/_Project_Files/Scripts/Managers/PlayerStateManager.cs
+++ Wizard BattleSim/Assets/_Project_Files/Scripts/Managers/PlayerStateManager.cs
@@ -8,7 +8,8 @@ using UnityEngine;
 public class PlayerStateManager : NetworkBehaviour
 {
     [SerializeField] public List<CharacterSelectState> AllStatePlayers = new List<CharacterSelectState>();
-    public static PlayerStateManager Singleton; private void Start()
+    public static PlayerStateManager Singleton; 
+    private void Start()
     {
         //singleton 
         if (Singleton == null)
@@ -19,14 +20,24 @@ public class PlayerStateManager : NetworkBehaviour
         {
             Destroy(this.gameObject);
         }
-        DontDestroyOnLoad(Singleton.gameObject);
+
 
     }
+
     public override void OnNetworkSpawn()
     {
+        DontDestroyOnLoad(Singleton.gameObject);
         NetworkManager.OnClientConnectedCallback += OnPlayerSpawn;
         NetworkManager.OnClientDisconnectCallback -= OnPlayerDespawn;
+
+
+        foreach (var client in NetworkManager.Singleton.ConnectedClientsList)
+        {
+            OnPlayerSpawn(client.ClientId);
+        }
     }
+
+
     public void OnPlayerSpawn(ulong ClientID)
     {
         var TempState = new CharacterSelectState(ClientID);
@@ -45,7 +56,7 @@ public class PlayerStateManager : NetworkBehaviour
     {
 
         var index = -1;
-        for(int i = 0; i< AllStatePlayers.Count; i++)
+        for (int i = 0; i < AllStatePlayers.Count; i++)
         {
             if (AllStatePlayers[i].ClientId == CSS.ClientId)
             {
@@ -64,14 +75,38 @@ public class PlayerStateManager : NetworkBehaviour
             // Get the existing state
             var existingState = AllStatePlayers[index];
 
-            // Update only non-default values amd values that are not the same as the existing state
-            if (    CSS.CharacterId != -1 || CSS.CharacterId != existingState.CharacterId) existingState.CharacterId = CSS.CharacterId;
-            if (CSS.WandID != -1 || CSS.WandID != existingState.WandID) existingState.WandID = CSS.WandID;
-            if (CSS.Spell0 != -1 || CSS.Spell0 != existingState.Spell0) existingState.Spell0 = CSS.Spell0;
-            if (CSS.Spell1 != -1 || CSS.Spell1 != existingState.Spell1) existingState.Spell1 = CSS.Spell1;
-            if (CSS.Spell2 != -1 || CSS.Spell2 != existingState.Spell2) existingState.Spell2 = CSS.Spell2;
 
-            existingState.IsLockedIn = CSS.IsLockedIn; 
+            if (existingState.CharacterId != CSS.CharacterId && CSS.CharacterId != -1)
+            {
+                existingState.CharacterId = CSS.CharacterId;
+            }
+            if (existingState.WandID != CSS.WandID && CSS.WandID != -1)
+            {
+                existingState.WandID = CSS.WandID;
+            }
+            if (existingState.Spell0 != CSS.Spell0 && CSS.Spell0 != -1)
+            {
+                existingState.Spell0 = CSS.Spell0;
+            }
+            if (existingState.Spell1 != CSS.Spell1 && CSS.Spell1 != -1)
+            {
+                existingState.Spell1 = CSS.Spell1;
+            }
+            if (existingState.Spell2 != CSS.Spell1 && CSS.Spell2 != -1)
+            {
+                existingState.Spell2 = CSS.Spell2;
+            }
+
+
+
+            /*            // Update only non-default values amd values that are not the same as the existing state
+                        if (    CSS.CharacterId != -1 || CSS.CharacterId != existingState.CharacterId) existingState.CharacterId = CSS.CharacterId;
+                        if (CSS.WandID != -1 || CSS.WandID != existingState.WandID) existingState.WandID = CSS.WandID;
+                        if (CSS.Spell0 != -1 || CSS.Spell0 != existingState.Spell0) existingState.Spell0 = CSS.Spell0;
+                        if (CSS.Spell1 != -1 || CSS.Spell1 != existingState.Spell1) existingState.Spell1 = CSS.Spell1;
+                        if (CSS.Spell2 != -1 || CSS.Spell2 != existingState.Spell2) existingState.Spell2 = CSS.Spell2;
+            */
+            existingState.IsLockedIn = CSS.IsLockedIn;
 
             // Update the list with the merged state
             AllStatePlayers[index] = existingState;
