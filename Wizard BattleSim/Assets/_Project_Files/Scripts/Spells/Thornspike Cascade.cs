8b18a2cd3c4acc995fa435d3b075d05c97614441
Noobie <jedstone567@gmail.com>
2024-12-27 15:16:34 -0500

    Thorn Cascade added

---
+++ Wizard BattleSim/Assets/_Project_Files/Scripts/Spells/Thornspike Cascade.cs
@@ -0,0 +1,31 @@
+using Unity.Mathematics;
+using Unity.Netcode;
+using UnityEngine;
+
+public class ThornspikeCascade : NetworkBehaviour, ISpell_Interface {
+    public Spell spell;
+
+    public ulong CasterId { get; set; }
+    public float hitagainTime { get; set; }
+
+    Spell ISpell_Interface.spell => throw new System.NotImplementedException();
+
+    public Vector3 Direction;
+    public float Offset = 5;
+    public void FireSpell() {
+
+
+    }
+
+    public void Initialize(ulong casterId, Vector3 direction) {
+
+        CasterId = casterId;
+        Direction = direction;
+        transform.LookAt(Direction);
+        transform.rotation = new quaternion(0, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + Offset);
+    }
+
+    public void TriggerEffect() {
+    }
+}
