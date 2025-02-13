a5c63b65c2f0a7d5c34c7cf3ef2b8131455307f8
Mr. Noob <jedstone567@gmail.com>
2025-02-13 16:39:55 -0500

    Changing to relay test_01

--- Wizard BattleSim/Assets/_Project_Files/Scripts/UI/ServermanagerUI.cs
+++ Wizard BattleSim/Assets/_Project_Files/Scripts/UI/ServermanagerUI.cs
@@ -11,7 +11,10 @@ public class ServermanagerUI : NetworkBehaviour
     [SerializeField] private Button startClientButton;
     private NetworkVariable<int> PLayerNum = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone);
     [SerializeField] private TextMeshProUGUI PLayerNumText;
-
+    testRelay testRelay;
+    public string joinCode;
+    public InputField InputWindow;
+    public TextMeshProUGUI joinCodeErrorText;
     private void Awake()
     {// ADD THIS SCRIPT TO CANVSA OBJECT
         startServerButton.onClick.AddListener(() =>
@@ -26,25 +29,44 @@ public class ServermanagerUI : NetworkBehaviour
 
         startClientButton.onClick.AddListener(() =>
         {
-            NetworkManager.Singleton.StartClient();
-            startClientButton.gameObject.SetActive(false);
-            startHostButton.gameObject.SetActive(false);
-            startServerButton.gameObject.SetActive(false);
-            PLayerNumText.gameObject.SetActive(false);
+            //NetworkManager.Singleton.StartClient();
+            //take in the input from user using input field
+            if (joinCode != "")
+            {
 
+
+                testRelay.JoinRelay(joinCode);
+
+                startClientButton.gameObject.SetActive(false);
+                startHostButton.gameObject.SetActive(false);
+                startServerButton.gameObject.SetActive(false);
+                PLayerNumText.gameObject.SetActive(false);
+            }
+            else
+            {
+                joinCodeErrorText.text = "Please enter a join code first";
+            }
         });
 
         startHostButton.onClick.AddListener(() =>
         {
-            NetworkManager.Singleton.StartHost();
+           // NetworkManager.Singleton.StartHost();
+           testRelay.CreateRelay();
             startClientButton.gameObject.SetActive(false);
             startHostButton.gameObject.SetActive(false);
             startServerButton.gameObject.SetActive(false);
             PLayerNumText.gameObject.SetActive(false);
 
         });
+
+        InputWindow.onEndEdit.AddListener((string s) =>
+        {
+            joinCode = s;
+        });
         
     }
+
+    
 
     private void Update()
     {
---
+++ Wizard BattleSim/Assets/_Project_Files/Scripts/relay/testRelay.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using Unity.Networking.Transport.Relay;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+
+public class testRelay : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    async void Start()
+    {//initialize the Unity Services
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+
+           Debug.Log("Signed in" + AuthenticationService.Instance.PlayerId);
+        };
+
+
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    //create a relay
+    async public void CreateRelay()
+    {
+        try
+        {
+            //create a relay with 2 slots
+           Allocation allocation =  await RelayService.Instance.CreateAllocationAsync(2);
+
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            Debug.Log("Join code: " + joinCode);
+
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(
+                allocation.RelayServer.IpV4,
+                (ushort)allocation.RelayServer.Port,
+                allocation.AllocationIdBytes,
+                allocation.Key,
+                allocation.ConnectionData
+                );
+
+            NetworkManager.Singleton.StartHost();
+        }
+        catch (Unity.Services.Relay.RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
+    
+    }
+
+    //join a relay
+    public async void JoinRelay(string joincode)
+    {
+        try
+        {
+            //join the relay with the join code provided
+            Debug.Log("Joining relay with join code: " + joincode);
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joincode);
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(
+                joinAllocation.RelayServer.IpV4,
+                (ushort)joinAllocation.RelayServer.Port,
+                joinAllocation.AllocationIdBytes,
+                joinAllocation.Key,
+                joinAllocation.ConnectionData,
+                joinAllocation.HostConnectionData
+                );
+
+            NetworkManager.Singleton.StartClient();
+        }
+
+        
+        catch (RelayServiceException e)
+        {//catch any exceptions
+            Debug.Log(e);
+        }
+
+    }
+}
